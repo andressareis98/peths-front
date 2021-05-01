3b7161a1f70523a6d40cfa35d1ffaec66c16a610
Andressa Reis Araujo <andressareis98@outlook.com>
2021-05-01 20:09:47 -0300

    feat: add vaccine integration with api

--- src/views/VaccineForm.js
+++ src/views/VaccineForm.js
@@ -3,6 +3,7 @@ import React, {useState} from 'react';
 import {View, Text, TextInput, Platform, TouchableOpacity} from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import moment from 'moment';
+import {CheckBox} from 'react-native-elements';
 
 import {server, showError, showSuccess} from '../common';
 import PetDetails from '../components/PetDetails';
@@ -11,11 +12,26 @@ export default ({route, navigation}) => {
   const [vaccine, setVaccine] = useState({
     data: new Date(),
     nome: '',
-    status: '',
+    status: 'Concluido',
     showDatePicker: false,
+    isApplyVaccine: true,
   });
 
   pet = route.params;
+
+  addVaccine = async vaccine => {
+    try {
+      await axios.post(`${server}/pets/${pet.id}/vaccines`, {
+        data: vaccine.data,
+        nome: vaccine.nome,
+        status: vaccine.status,
+      });
+    } catch (e) {
+      showError(e);
+    }
+  };
+
+  console.warn(pet);
 
   getDatePicker = () => {
     let datePicker = (
@@ -47,10 +63,49 @@ export default ({route, navigation}) => {
   };
 
   return (
-    <View>
+    <View style={{flex: 1}}>
       <PetDetails pet={pet} />
-      <Text>Data:</Text>
-      {this.getDatePicker()}
+      <Text>Ação: </Text>
+      <View style={{flexDirection: 'row'}}>
+        <CheckBox
+          center
+          title="Aplicar Vacina"
+          checkedIcon="dot-circle-o"
+          uncheckedIcon="circle-o"
+          checked={vaccine.isApplyVaccine}
+          onPress={() =>
+            setVaccine({...vaccine, isApplyVaccine: true, status: 'Concluído'})
+          }
+        />
+        <CheckBox
+          center
+          title="Agendar Vacina"
+          checkedIcon="dot-circle-o"
+          uncheckedIcon="circle-o"
+          checked={!vaccine.isApplyVaccine}
+          onPress={() =>
+            setVaccine({...vaccine, isApplyVaccine: false, status: 'Pendente'})
+          }
+        />
+      </View>
+
+      {!vaccine.isApplyVaccine && <Text>Data:</Text>}
+      {!vaccine.isApplyVaccine && this.getDatePicker()}
+
+      <Text>Vacina</Text>
+      <TextInput
+        onChangeText={nome => setVaccine({...vaccine, nome})}
+        placeholder="Informe o nome"
+        value={vaccine.nome}
+      />
+
+      <TouchableOpacity
+        onPress={() => {
+          this.addVaccine(vaccine);
+          navigation.goBack();
+        }}>
+        <Text>Salvar</Text>
+      </TouchableOpacity>
     </View>
   );
 };
