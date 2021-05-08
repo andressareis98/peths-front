77b1d25860cd3477514ba645507076729da0e297
Andressa Reis Araujo <andressareis98@outlook.com>
2021-05-08 11:35:08 -0300

    feat: add qrcode

--- src/components/PetDetails.js
+++ src/components/PetDetails.js
@@ -1,18 +1,43 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import {Avatar} from 'react-native-elements';
+import QRCode from 'react-native-qrcode-svg';
 import commonStyles from '../commonStyles';
 
 import PetAge from '../components/PetAge';
 
 export default ({pet}) => {
+  const [setAvatar, avatar] = useState(true);
+
+  const changeVisualizationAvatar = () => {};
   return (
     <View style={styles.container}>
-      <Avatar
-        avatarStyle={{borderRadius: 10}}
-        size="large"
-        source={{uri: pet.avatarUrl}}
-      />
+      <View>
+        <Avatar
+          avatarStyle={{borderRadius: 10}}
+          size="large"
+          source={{uri: pet.avatarUrl}}
+        />
+      </View>
+
+      <View>
+        <QRCode
+          value={`${pet.id}`}
+          size={100}
+          color="black"
+          backgroundColor="white"
+        />
+      </View>
+
+      {/* <TouchableOpacity
+        onPress={() =>
+          avatar ? setAvatar({'QrCode'}) : setAvatar('Avatar')
+        }>
+        <Text>Trocar para {avatar == 'Avatar' ? 'QrCode' : 'foto'}</Text>
+      </TouchableOpacity>
+
+      <Text>{avatar}</Text> */}
+
       <View style={styles.containerDetails}>
         <View style={styles.containerData}>
           <Text style={styles.title}>ID: </Text>
@@ -56,6 +81,9 @@ const styles = StyleSheet.create({
     padding: 10,
     flexDirection: 'row',
     alignItems: 'center',
+  },
+  avatar: {
+    marginRight: 10,
   },
   containerDetails: {
     marginLeft: 10,
